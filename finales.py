aa42d00729dbfbcdee709561c4be0181a2190a94
Marina Evangelista <marinaevangelista08@gmail.com>
2024-06-24 16:17:26 -0300

    Clase 11 Phyton

---
+++ finales.py
@@ -0,0 +1,10 @@
+"""
+¿Qué es el pip y porque lo actualizamos?
+
+Pip es una herramienta esencial para cualquier desarrollador de Python. 
+Permite instalar, actualizar y administrar paquetes de software con facilidad, lo que a su vez facilita el desarrollo 
+y la distribución de aplicaciones y proyectos de Python.
+
+Además, pip hace que sea fácil compartir código y colaborar con otros desarrolladores en la comunidad de Python.
+
+"""
